6cdafba65642df4a00d2c913a9991662f1b033eb
Cristopher <CristopherDLCH@gmail.com>
2020-07-21 15:45:44 -0500

    Registrar nuevos productos

--- modulos/nuevo.php
+++ modulos/nuevo.php
@@ -39,7 +39,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <title>Nuevo Producto</title>
+    <title>Nuevo</title>
 </head>
 <body>
     <div class="container">
